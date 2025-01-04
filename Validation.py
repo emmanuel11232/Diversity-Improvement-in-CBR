3583ed23f3e85328bb0f82f928312c5e4b364a6f
emmanuel11232 <emmanuel111232@gmail.com>
2025-01-03 22:34:25 -0600

    Comparisons with original diversity

--- Validation.py
+++ Validation.py
@@ -22,6 +22,7 @@ import Levenshtein
 import random
 
 from Methods2 import CasoInd,Diversity,ModifiedCNN,SearchSimilarModCNN, DescriptionsAndSolutions
+from Methods import SearchSimilar
 #First I will set the original case base
 #Case Base comes from an Excel provided by the supervisor
 path = r'C:\Users\emman\Documents\TEC\DLIG\Case Based Reasoning\CaseBase\CleanedDATA V12-05-2021.csv'
@@ -41,6 +42,9 @@ CaseBase_Test=CaseBase[:40] #Set to make the queries
 shared_data = {}
 shared_data['Diversity_local'] = []
 shared_data['Diversity_average'] = []
+
+shared_data['Diversity_local_Org'] = []
+shared_data['Diversity_average_Org'] = []
 
 def apply_algorithms(Threshold):
     global shared_data
@@ -62,24 +66,40 @@ def save_data(values):
 
     ListRetrievals=SearchSimilarModCNN(values,Nested_Descriptions,Nested_Solutions,NumberRetrievals,Weights)
 
-    for i in range(len(ListRetrievals)):
-        print(f"Solución {i} : ", ListRetrievals[i].solution,"\n")
-
     # Función para mostrar la información de los objetos en la GUI
     Div=Diversity(ListRetrievals,Weights)
     print("Diversidad de soluciones:", Div)
     print(len(Nested_Descriptions),len(Nested_Solutions))
     return Div
 
+def Original_diversity(values):
+    Weights= [0.2, 0.2, 0.2, 0.2, 0.2] 
+    ListRetrievals,ListSim=SearchSimilar(values,CaseBase,5,Weights)
+
+    # Función para mostrar la información de los objetos en la GUI
+    Div=Diversity(ListRetrievals,Weights)
+
+    return Div
+
+def Diversity_Calculations_Original():
+    global shared_data
+    Divs=[]
+    for i in range(0,40):
+        values=CaseBase_Test[i].description
+        Div=Original_diversity(values)
+        Divs.append(Div)
+    shared_data['Diversity_local_Org'] = Divs
+    shared_data['Diversity_average_Org'].append(np.mean(Divs))
+    print(f"Original Diversity: {np.mean(Divs)}")
+
 def Diversity_Calculations():
     global shared_data
-    Thresholds=[0.5,0.6,0.7,0.8,0.9,0.95]
+    Thresholds=[0.4,0.5,0.6,0.7,0.8,0.9,0.95]
     for j in Thresholds:
         apply_algorithms(j)
         Divs=[]
         for i in range(0,40):
             values=CaseBase_Test[i].description
-            print(values)
             Div=save_data(values)
             Divs.append(Div)
         shared_data['Diversity_local'] = Divs
@@ -88,7 +108,7 @@ def Diversity_Calculations():
 
 def plot_diversity():
     global shared_data
-    Thresholds=[0.5,0.6,0.7,0.8,0.9,0.95]
+    Thresholds=[0.4,0.5,0.6,0.7,0.8,0.9,0.95]
     Divs=shared_data.get('Diversity_local', [])
     Divs_average=shared_data.get('Diversity_average', [])
     plt.plot(Thresholds,Divs_average)
@@ -99,4 +119,5 @@ def plot_diversity():
     print(f"Average Diversity: {Divs_average}")
 
 Diversity_Calculations()
+Diversity_Calculations_Original()
 plot_diversity()
